e0ad13f49ba47914c433b6a4ee417b455fa1d718
eddsonjose <44173160+eddsonjose@users.noreply.github.com>
2019-10-01 17:46:50 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,8 +1,14 @@
+let strokeSize = 10;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(220);
+  strokeWeight(strokeSize);
 }
 
 function draw() {
-  background(220);
-  text("hello world", 100, 100);
+  if (mouseIsPressed) {
+    stroke(random(100, 200), random(100, 200), random(100, 200));
+    line(mouseX, mouseY, 1.2*pmouseX, 1.2*pmouseY);
+  }
 }

50db09c7f85f88f9641433061ff757a0b2bccf8c
eddsonjose <44173160+eddsonjose@users.noreply.github.com>
2019-10-02 21:16:05 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,14 +1,29 @@
-let strokeSize = 10;
+let strokeSize = 0.01;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(220);
+  createCanvas(windowWidth*0.9, windowHeight*0.8);
   strokeWeight(strokeSize);
+  background(0);
 }
-
+//------------------------------------------------------------------
 function draw() {
   if (mouseIsPressed) {
-    stroke(random(100, 200), random(100, 200), random(100, 200));
-    line(mouseX, mouseY, 1.2*pmouseX, 1.2*pmouseY);
+    stroke(random(200, 255), random(200, 255), random(200, 255));
+    line(width - mouseX, height - mouseY, pmouseX, pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    noStroke();
+    fill(random(200, 255), random(200, 255), random(200, 255));
+    circle(width - mouseX, height - mouseY, random(1, 4));
+    circle(mouseX, mouseY, random(1, 4));
+
+
   }
+}
+
+function keyTyped() {
+  if (key === 's') {
+    console.log("Image saved");
+    saveCanvas('fileName', 'png');
+  }
+  return false;
 }

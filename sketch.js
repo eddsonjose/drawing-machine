5def2b7d84d49c8beee9488921f6d90528533c38
eddsonjose <44173160+eddsonjose@users.noreply.github.com>
2019-10-03 08:45:48 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,22 +1,22 @@
 let strokeSize = 0.01;
+let circleSize = 4;
 
 function setup() {
   createCanvas(windowWidth*0.9, windowHeight*0.8);
-  strokeWeight(strokeSize);
   background(0);
 }
 //------------------------------------------------------------------
 function draw() {
+  strokeWeight(strokeSize);
+
   if (mouseIsPressed) {
     stroke(random(200, 255), random(200, 255), random(200, 255));
     line(width - mouseX, height - mouseY, pmouseX, pmouseY);
     line(mouseX, mouseY, pmouseX, pmouseY);
     noStroke();
     fill(random(200, 255), random(200, 255), random(200, 255));
-    circle(width - mouseX, height - mouseY, random(1, 4));
-    circle(mouseX, mouseY, random(1, 4));
-
-
+    circle(width - mouseX, height - mouseY, random(1, circleSize));
+    circle(mouseX, mouseY, random(1, circleSize));
   }
 }
 
@@ -25,5 +25,17 @@ function keyTyped() {
     console.log("Image saved");
     saveCanvas('fileName', 'png');
   }
+  if (key === '=') {
+    console.log("Stroke weight increased");
+    console.log(strokeSize);
+    strokeSize++;
+    circleSize++;
+  } else if (key === '-') {
+    console.log("Stroke weight decreased");
+    console.log(strokeSize);
+    strokeSize--;
+    circleSize--;
+  }
   return false;
+
 }

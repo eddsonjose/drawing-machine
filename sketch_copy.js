ee306ebe545681be17604f8ff2a998c2a5cead71
eddsonjose <44173160+eddsonjose@users.noreply.github.com>
2019-10-06 17:41:26 -0700

    update

--- sketch_copy.js
+++ sketch_copy.js
@@ -1,8 +1,6 @@
 let strokeSize = 0.01;
 let circleSize = 2;
 let noiseValue = 0.0;
-let bigBang = 0;
-
 
 function setup() {
   createCanvas(windowWidth*0.9, windowHeight*0.8);
@@ -13,34 +11,24 @@ function draw() {
 
   // frameRate(1);
 
-  let n = noise(noiseValue) * width;
-  noiseValue += 0.1;
-  if (noiseValue >= 1) {
-    noiseValue = 0;
-  }
+  strokeWeight(strokeSize);
+  let n = noise(noiseValue) * random(0, 255);
 
   if (mouseIsPressed) {
-    strokeWeight(0.01);
     stroke(random(200, 255), random(200, 255), random(200, 255));
     line(width - mouseX, height - mouseY, pmouseX, pmouseY);
-    strokeWeight(strokeSize);
     line(mouseX, mouseY, pmouseX, pmouseY);
-    strokeWeight(0.01);
-    // fill(0, 0, 0, 0)
-    // ellipse(width/2, height/2, mouseX, mouseY);
+    fill(0, 0, 0, 0)
+    ellipse(width/2, height/2, mouseX, mouseY);
     //large stars
     noStroke();
     fill(random(150, 255), random(150, 255), random(150, 255));
-    circle(width - mouseX + random(-n/8, n/8), height - mouseY + random(-n/8, n/8), random(1, circleSize));
-    circle(mouseX + random(-n/8, n/8), mouseY + random(-n/8, n/8), random(1, circleSize));
+    circle(width - mouseX + random(-20, 20), height - mouseY + random(-20, 20), random(1, circleSize));
+    circle(mouseX + random(-20, 20), mouseY + random(-20, 20), random(1, circleSize));
+    //small stars
+    fill(255, 255, 255, random(230, n));
+    circle(random(0, width), random(0, height), 1);
   }
-  fill(0);
-  bigBang += bigBang;
-  circle(width/2, height/2, bigBang);
-  if (bigBang >= width*2) {
-    bigBang = 0;
-  }
-
 }
 //------------------------------------------------------------------
 function keyTyped() {
@@ -51,16 +39,13 @@ function keyTyped() {
   if (key === '=') {
     console.log("Stroke weight increased");
     console.log(strokeSize);
-    strokeSize += 0.5;
+    strokeSize += 0.05;
     circleSize += 0.5;
   } else if (key === '-') {
     console.log("Stroke weight decreased");
     console.log(strokeSize);
-    strokeSize -= 0.5;
+    strokeSize -= 0.05;
     circleSize -= 0.5;
-  }
-  if (key === 'r') {
-    bigBang = 1;
   }
   return false;
 }

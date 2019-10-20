372fbc5ddeb22ce21f1e2a325db491745b228fe0
eddsonjose <44173160+eddsonjose@users.noreply.github.com>
2019-10-20 12:45:43 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ let circleSize = 3;
 let bigBang = 0;
 let textXPos;
 let paragraph;
+let slider;
 //------------------------------------------------------------------
 function setup() {
   paragraph = createElement('p', 'What humans perceive as artificial is truly natural in a universal outlook because everything in the universe is made up of the same material – stardust. The human body, the mind, and its creations are a product of nature. What is unnatural is simply another name for man-made. Constellations are a human construct that we view as an object to believe in. Drawing with the stars will help change the scale of our perception of nature into the scale of the universe. Gender is another construct of humanity. Many say that there is naturally only two genders and anything more is considered unnatural. I say let there be a thousand genders because what makes a gender is what makes a human and what makes a human is nature. Let us believe what we want, let us form the stars into shapes, and let us be who we desire to be. Your drawing is what you believe in and your belief is natural.');
@@ -12,9 +13,15 @@ function setup() {
   background(0);
   frameRate(8);
   textXPos = width;
+  slider = createSlider(0, 40, 5, 1);
+  slider.position(width * 0.9, height + 120);
 }
 //------------------------------------------------------------------
 function draw() {
+  //slider for strokesize
+  let value = slider.value();
+  strokeSize = value;
+  circleSize = value;
   //scrolling paragraph outside of canvas
   paragraph.size(9000, 15);
   paragraph.style('text-align:right');
@@ -37,10 +44,6 @@ function draw() {
   if (bigBang >= width*2) {
     bigBang = 0;
   }
-  //scrolling paragraph inside the canvas
-  // rect(textXPos, 5, 9000, 25);
-  // fill(255);
-  // text('What humans percieve as artificial is truly natural in a universal outlook because everything in the universe is made up of the same material - stardust. The human body, the mind, and its creations are a product of nature. What is unnatural is simply another name for manmade. Gender is another construct of humanity. Many say that there is naturally only two genders and anything more is considered unnatural. I say let there be a thousand genders because what makes a gender is what makes a human and what makes a human is nature. Let us believe what we want, let us form the stars into shapes, and let us be who we desire to be. Keep drawing. Embellish it. Your drawing is what you believe in and your belief is natural.', textXPos, 20);
 }
 //------------------------------------------------------------------
 //form a star on mouse press
